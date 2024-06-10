ce08327e6d6da7931fa4a54aa233a96973c70438
Chayse McMillan <chaysemcmillan@Reciprotech.local>
2024-06-09 23:42:03 -0600

    Initial Commit

---
+++ src/Public.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Container from './Container';
+
+function Public() {
+    return (
+        <Container>
+            <h1>Public Route</h1>
+        </Container>
+    )
+}
+export default Public;
